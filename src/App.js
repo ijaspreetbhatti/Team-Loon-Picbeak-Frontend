11e2af01f9e1dbc84577e15e291a883304efebde
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-02-08 14:44:01 -0800

    Fix: Basic Folder Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Creating Picbeak with React!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
