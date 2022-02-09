eff10e0c23e7452414113ea5adf615bdd8a8d1c3
olimarie21 <olivia.underdah@gmail.com>
2022-02-08 18:15:08 -0800

    Fix: Remove duplicate files

--- picbeak/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('show test page', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
