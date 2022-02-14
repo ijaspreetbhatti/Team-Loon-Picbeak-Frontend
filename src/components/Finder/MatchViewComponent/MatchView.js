91d850efa8506d9be7a100598035f254849b914d
olimarie21 <olivia.underdah@gmail.com>
2022-02-14 11:58:08 -0800

    MatchView component style and function testing

--- src/components/Finder/MatchViewComponent/MatchView.js
+++ src/components/Finder/MatchViewComponent/MatchView.js
@@ -42,20 +42,24 @@ function MatchView() {
                 alert('Working button!');
             }
 
-
             return (
-            <div>
+            <div id="matchView">
                 <h2>Explore birds and spot the one you're spying!</h2>
                 <h6>Showing birds around @location</h6>
 
                 {items.map(item => (
-                    <div key={item.sciName} className='matchViewCard'>
-                        <img src="https://images.unsplash.com/photo-1616878457386-e09e41105743?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2831&q=80" alt="bird"></img>
-                        <h2>{item.comName}</h2>
-                        <p>{item.sciName}</p>
-                        <Button className='primary' onClick={displayBirdDetails}>This is the one!</Button>
-
-                        <AudioPlayer className="audio" url={`https://xeno-canto.org/api/2/recordings?query=${item.sciName}`}/>
+                    <div key={item.sciName} className='matchViewCard' id={item.sciName.replace(/\s/g, '-')}>
+                            <BirdImage url={`https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=8391ac68237f2756f302320f6e04fc66&tags=%27+${item.sciName}+%27&jsoncallback=?`}/>
+                        <div className="matchDetailCard">
+                            <div className="nameContainer">
+                                <h2>{item.comName}</h2>
+                                <p>{item.sciName}</p>
+                            </div>
+                            <div className="buttonContainer">
+                                <AudioPlayer className="audio" url={`https://xeno-canto.org/api/2/recordings?query=${item.sciName}`}/>
+                                <Button className='primary-small matchCardBtn' onClick={displayBirdDetails}>This is the one!</Button>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
@@ -63,58 +67,70 @@ function MatchView() {
         }
 }
 
-function AudioPlayer(props) {
 
-    function playSound() {
-        const birdSound = new Audio(props.url);
-        birdSound.play();
-        console.log('playing');
-    }
-            return (
-                <button url={props.url} className={props.className} onClick={playSound} >Play</button>
+// https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+function BirdImage(props) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [images, setImages] = useState([]);
+
+    const url = String(props.url.replace(/\s/g, '+'));
+
+    // const requestOptions = {
+    //     method: 'GET',
+    //     redirect: 'follow'
+    // };
+
+    useEffect(() => {
+        fetch(url) 
+            .then(result => result.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setImages(result);
+                },
+
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+        }, [])
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+
+        // const src = `https://live.staticflickr.com/${items.photos.photo[0].server}/${items.photos.photo[0].id}_${items.photos.photo[0].secret}.jpg`
+    
+        return (
+            <p>image here {String(images)}</p>
             )
         }
+    }
+    // <img src="" url={props.url} />
+
+
+
+function AudioPlayer(props) {
+    function playSound() {
+        // const birdSound = new Audio(items.recordings[0].file);
+        console.log("this");
+        // birdSound.play();
+    }
+    // function playSound() {
+    //     const birdSound = new Audio(props.url);
+    //     birdSound.play();
+    //     console.log('playing');
     // }
-// }
+    return (
+        <button url={props.url} className={props.className} onClick={playSound} >Play</button>
+    )
+    //     }
+    // }
+}
 
 export default MatchView; 
-
-
-// const [error, setError] = useState(null);
-    // const [isLoaded, setIsLoaded] = useState(false);
-    // const [items, setItems] = useState([]);
-
-    // let headers = new Headers();
-
-    // headers.append('Content-Type', 'application/json');
-    // headers.append('Accept', 'application/json');
-
-    // const url = props.url.replace(/\s/g, '+');
-
-    // useEffect(() => {
-    //     fetch(`${url}`, headers) 
-    //         .then(res => res.json())
-    //         .then(
-    //             (result) => {
-    //                 setIsLoaded(true);
-    //                 setItems(result);
-    //             },
-
-    //             (error) => {
-    //                 setIsLoaded(true);
-    //                 setError(error);
-    //             }
-    //         )
-    //     }, [])
-
-    //     if (error) {
-    //         return <div>Error: {error.message}</div>;
-    //     } else if (!isLoaded) {
-    //         return <div>Loading...</div>;
-    //     } else {
-
-    //         function playSound() {
-    //             // const birdSound = new Audio(items.recordings[0].file);
-    //             console.log("this" + items);
-    //             // birdSound.play();
-    //         }
