e9309d97f00c9ce8cf569e263076fe9d26314521
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-02-14 12:04:32 +0900

    test BgDetails

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 // import logo from './logo.svg';
-import './App.css';
-import Button from './components/shared/ButtonComponent/Button';
+import "./App.css";
+import Button from "./components/shared/ButtonComponent/Button";
+
+import BgDetails from "./components/HomeComponent/BgDetails";
 
 function App() {
-  const buttonCheckHandler = () => {
-    alert('Working!!!!');
-  }
+    const buttonCheckHandler = () => {
+        alert("Working!!!!");
+    };
 
-  return (
-    <div className="App">
-      <Button className="primary" onClick={buttonCheckHandler}>Identify bird</Button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button className="primary" onClick={buttonCheckHandler}>
+                Identify bird
+            </Button>
+
+            <BgDetails></BgDetails>
+        </div>
+    );
 }
 
 export default App;
