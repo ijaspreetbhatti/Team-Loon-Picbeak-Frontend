33601e41a2dc2136bafc2b3b1ff133c1789f59ee
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-02-16 14:28:21 +0900

    first commit

---
+++ src/components/DiscoverComponent/BirdFilter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BirdFilter() {
+    return (
+        <div className="bird-filter">
+            <input type="text" id="searchInput" placeholder="enter bird name" />
+        </div>
+    );
+}
+
+export default BirdFilter;
