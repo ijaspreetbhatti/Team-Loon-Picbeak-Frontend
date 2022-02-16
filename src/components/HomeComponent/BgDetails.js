d1686334af3e1480f87fc3fc480f584397dac3e6
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-02-16 14:29:04 +0900

    update p tag

--- src/components/HomeComponent/BgDetails.js
+++ src/components/HomeComponent/BgDetails.js
@@ -6,7 +6,10 @@ import Population from "./Population";
 const BgDetails = (props) => {
     return (
         <div>
-            <p>Why we need birds to support a healthy environment</p>
+            <p>
+                More than a connection to nature, birds supports a healthy
+                environment
+            </p>
             <Block />
             <Block />
             <Block />
