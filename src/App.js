3ad983442ac82cdc097968b30e9557519db7be16
Gavriil84 <gabwt.84@gmail.com>
2022-02-20 20:47:21 -0800

    add: useState for Login

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import Button from "./components/shared/ButtonComponent/Button";
 import BgDetails from "./components/HomeComponent/BgDetails";
 
 import BirdFilter from "./components/DiscoverComponent/BirdFilter";
+import Card from "./components/shared/DialogComponent/Card";
+import Login from "./components/LoginComponent/Login/Login";
+import { useEffect, useState } from "react";
 
 function App() {
-    const buttonCheckHandler = () => {
-        alert("Working!!!!");
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const userInfo = localStorage.getItem('loggedIn');
+        if (userInfo === '1') {
+            setLoggedIn(true);
+            alert('You have logged in, Welcome :)');
+        }
+    }, []);
+
+    const loginHandler = (email, password) => {
+        localStorage.setItem('loggedIn', '1')
+        setLoggedIn(true);
+    }
+
+    const logoutHandler = () => {
+        localStorage.removeItem('loggedIn')
+        setLoggedIn(false);
     };
 
     return (
         <div className="App">
-            <Button className="primary" onClick={buttonCheckHandler}>
-                Identify bird
-            </Button>
-
-            <BgDetails></BgDetails>
-            <BirdFilter></BirdFilter>
+            {!loggedIn && <Login onLogin={loginHandler} />}
+            {/* {isLoggedIn && <Home onLogout={logoutHandler} />} */}
         </div>
     );
 }
