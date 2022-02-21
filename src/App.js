469d3bd3ff9a90ead7cf96a380cfec3ba2426080
Gavriil84 <gabwt.84@gmail.com>
2022-02-20 21:42:52 -0800

    update: change import file from css to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import "./App.css";
+import "./App.scss";
 import Button from "./components/shared/ButtonComponent/Button";
 
 import BgDetails from "./components/HomeComponent/BgDetails";
