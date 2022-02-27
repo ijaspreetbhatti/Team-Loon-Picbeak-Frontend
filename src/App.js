1877dc1d8725c4576f0f1b8d4daea74708e9ea4d
olimarie21 <olivia.underdah@gmail.com>
2022-02-27 11:15:35 -0800

    feat: add additional routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.scss";
 import Login from "./components/LoginComponent/Login/Login";
 import { useEffect, useState } from "react";
 import "./App.scss";
-// import MatchView from "./components/Finder/MatchViewComponent/MatchView";
+import MatchView from "./components/Finder/MatchViewComponent/MatchView";
+import ListView from "./components/Finder/ListViewComponent/ListView";
 import Header from "./components/shared/HeaderComponent/Header";
 import FooterComponent from "./components/shared/FooterComponent/FooterComponent";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -40,8 +41,9 @@ function App() {
                 <Routes>
                     <Route index element={<HomeComponent />} />
                     <Route path="donate" element={<DonateComponent />} />
-                    {/* <Route path="match" element={<MatchView />} /> */}
+                    <Route path="match" element={<MatchView />} />
                     <Route path="*" element={<HomeComponent />} />
+                    <Route path="listview" element={<ListView />} />
                 </Routes>
             </Router>
             {/* <Login></Login> */}
