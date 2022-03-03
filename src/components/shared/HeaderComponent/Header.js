350e0ad424f78897e395f40dbb8bfa7e6afa8ec0
olimarie21 <olivia.underdah@gmail.com>
2022-03-03 10:27:52 -0800

    refactor header class to function components

--- src/components/shared/HeaderComponent/Header.js
+++ src/components/shared/HeaderComponent/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Header.scss";
+import MainMenu from './MainMenu';
+import ProfileMenu from './ProfileMenu';
 
-function Header() {
+export default function Header() {
     return (
         <div className="headerContainer">
             <a href="*">
@@ -15,107 +17,9 @@ function Header() {
                     <a href="/discover">Discover Birds</a>
                     <a href="/donate">Donate</a>
                 </div>
-                <Menu />
+                <MainMenu />
                 <ProfileMenu />
             </div>
         </div>
     );
 }
-
-class Menu extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            showMenu: false,
-        };
-
-        this.showMenu = this.showMenu.bind(this);
-        // this.closeMenu = this.closeMenu.bind(this);
-    }
-
-    showMenu(event) {
-        event.preventDefault();
-
-        if (!this.state.showMenu) {
-            this.setState({ showMenu: true });
-        } else {
-            this.setState({ showMenu: false });
-        }
-    }
-
-    render() {
-        return (
-            <div className="menuContainer">
-                <div
-                    onClick={this.showMenu}
-                    className={
-                        this.state.showMenu ? "active-nav" : "navMenuIcon"
-                    }
-                ></div>
-                {this.state.showMenu ? (
-                    <div
-                        className="navMenu"
-                        ref={(element) => {
-                            this.dropdownMenu = element;
-                        }}
-                    >
-                        <a href="*" id="homeBtn">
-                            Home
-                        </a>
-                        <a href="/match">Identify Birds</a>
-                        <a href="/discover">Discover Birds</a>
-                        <a href="/donate">Donate</a>
-                    </div>
-                ) : null}
-            </div>
-        );
-    }
-}
-
-class ProfileMenu extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            showMenu: false,
-        };
-
-        this.showMenu = this.showMenu.bind(this);
-        // this.closeMenu = this.closeMenu.bind(this);
-    }
-
-    showMenu(event) {
-        event.preventDefault();
-
-        if (!this.state.showMenu) {
-            this.setState({ showMenu: true });
-        } else if (this.state.showMenu) {
-            this.setState({ showMenu: false });
-        }
-    }
-
-    render() {
-        return (
-            <div className="profileMenuContainer">
-                <div
-                    onClick={this.showMenu}
-                    className={this.state.showMenu ? "active" : "profileLink"}
-                ></div>
-                {this.state.showMenu ? (
-                    <div
-                        className="profileMenu"
-                        ref={(element) => {
-                            this.dropdownMenu = element;
-                        }}
-                    >
-                        <a href="*">Profile</a>
-                        <a href="*">Log out</a>
-                    </div>
-                ) : null}
-            </div>
-        );
-    }
-}
-
-export default Header;
---
+++ src/components/shared/HeaderComponent/MainMenu.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Header.scss";
+
+export default function MainMenu() {
+    const [showMenu, setMenu] = useState(false);
+
+    const changeMenu = (event) => {
+        event.preventDefault();
+
+        if (!showMenu) {
+            setMenu(true);
+        } else if(showMenu) {
+            setMenu(false);
+        }
+    }
+
+    return (
+        <div className="menuContainer">
+            <div
+                onClick={changeMenu}
+                className={
+                    showMenu ? "active-nav" : "navMenuIcon"
+                }
+            ></div>
+            {showMenu ? (
+                <div
+                    className="navMenu"
+                >
+                    <a href="*" id="homeBtn">
+                        Home
+                    </a>
+                    <a href="/match">Identify Birds</a>
+                    <a href="/discover">Discover Birds</a>
+                    <a href="/donate">Donate</a>
+                </div>
+            ) : null}
+        </div>
+    );
+}
