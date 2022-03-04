b9463dc9cb23d0ef2c3ff1b6c4fa5b261143f591
Gavriil84 <gabwt.84@gmail.com>
2022-03-03 20:00:32 -0800

    update: set useState for login modal

--- src/components/shared/HeaderComponent/ProfileMenu.js
+++ src/components/shared/HeaderComponent/ProfileMenu.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
+import Login from "../../LoginComponent/Login/Login";
 import "./Header.scss";
 
 export default function ProfileMenu() {
     const [showMenu, setMenu] = useState(false);
+    const [loginModal, setLoginModal] = useState(false);
 
     const changeMenu = (event) => {
         event.preventDefault();
 
         if (!showMenu) {
             setMenu(true);
-        } else if(showMenu) {
+        } else if (showMenu) {
             setMenu(false);
         }
     }
+
+    const showLoginModal = () => {
+        setLoginModal(true);
+        setMenu(false);
+    }
+
+
 
     return (
         <div className="profileMenuContainer">
@@ -24,10 +33,11 @@ export default function ProfileMenu() {
             ></div>
             {showMenu ? (
                 <div className="profileMenu">
-                        <a href="*">Profile</a>
-                        <a href="*">Log out</a>
-                    </div>
+                    <button>Profile</button>
+                    <button onClick={showLoginModal}>Log in</button>
+                </div>
             ) : null}
+            <Login onClose={() => setLoginModal(false)} show={loginModal} />
         </div>
     );
 }
