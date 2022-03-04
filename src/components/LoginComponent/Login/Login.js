e07371947ad6071d59c997a5f143408dcf2f78b3
Gavriil84 <gabwt.84@gmail.com>
2022-03-04 15:32:43 -0800

    update: add sign up component

--- src/components/LoginComponent/Login/Login.js
+++ src/components/LoginComponent/Login/Login.js
@@ -5,6 +5,7 @@ import Card from '../../shared/DialogComponent/Card';
 import "./Login.scss";
 
 function Login(props) {
+    const [changeModal, setChangeModal] = useState(false);
 
     if (!props.show) {
         return null;
@@ -14,33 +15,74 @@ function Login(props) {
         <div>
             <div className="modalbg" onClick={props.onClose}></div>
             <Card>
-                <form>
-                    <div className="modalHeader">
-                        <h1>Log in</h1>
-                        <Button className="exit" onClick={props.onClose}></Button>
-                    </div>
-                    <div className="emailWrapper">
+                {!changeModal ? (
+                    <form>
+                        <div className="modalHeader">
+                            <h1>Log in</h1>
+                            <Button className="exit" onClick={props.onClose}></Button>
+                        </div>
+                        <div className="emailWrapper">
 
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email" />
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email" />
 
-                    </div>
-                    <div className="passwordWrapper" >
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password" />
-                        <a href="/">Forgot password?</a>
-                    </div>
-                    <div className="buttonWrapper">
-                        <Button type="submit" className="primary">Log in</Button>
-                    </div>
-                    <span>New to Picbeak? <a href="/">Creat account</a></span>
-                </form>
+                        </div>
+                        <div className="passwordWrapper" >
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password" />
+                            <a href="/">Forgot password?</a>
+                        </div>
+                        <div className="buttonWrapper">
+                            <Button type="submit" className="primary">Log in</Button>
+                        </div>
+                        <span>New to Picbeak? <a onClick={() => setChangeModal(true)}>Creat account</a></span>
+                    </form>
+                ) : (
+                    <form >
+                        <div className="modalHeader">
+                            <h1>Create Account</h1>
+                            <Button className="exit" onClick={props.onClose}></Button>
+                        </div>
+
+                        <div className="nicknameWrapper">
+
+                            <label htmlFor="email">Nickname</label>
+                            <input
+                                type="text"
+                                id="nickname"
+                                name="nickname" />
+                        </div>
+
+                        <div className="emailWrapper2">
+
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email" />
+                        </div>
+
+                        <div className="passwordWrapper">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password" />
+                        </div>
+
+                        <div className="buttonWrapper">
+                            <Button type="submit" className="primary">Log in</Button>
+                        </div>
+                        <span>Already have an account? <a onClick={() => setChangeModal(false)}>Log in</a></span>
+                    </form>
+                )}
+
             </Card>
         </div>
     )
