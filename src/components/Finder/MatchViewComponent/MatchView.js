106c860731c35dfed1b869b1f3777d857209328a
olimarie21 <olivia.underdah@gmail.com>
2022-03-06 13:30:20 -0800

    testing: api calls for bird data

--- src/components/Finder/MatchViewComponent/MatchView.js
+++ src/components/Finder/MatchViewComponent/MatchView.js
@@ -8,29 +8,30 @@ import "swiper/css";
 import "swiper/css/navigation";
 import MatchCard from "./MatchCard";
 import axios from 'axios';
+import Audio from '../../shared/AudioComponent/Audio';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function MatchView() {
     const [birdsData, setBirdData] = useState([]);
     
-    // const birdDataUpdate = useCallback(() => {
-    //     setBirdData(birdsData);
-    // }, [birdsData]);
+    const birdDataUpdate = useCallback(() => {
+        setBirdData(birdsData);
+    }, [birdsData]);
     
-    useEffect(() => {
-        async function getImage(sciName, birdRef) {
-            const imgData = await axios.get(`https://pic-beak-backend.herokuapp.com/api/v1/birds/${sciName}/image`);
-            if(imgData) {
-                birdRef.imageLink = imgData.data.imageLink;
-            }
+    async function getImage(sciName, birdRef) {
+        const imgData = await axios.get(`https://pic-beak-backend.herokuapp.com/api/v1/birds/${sciName}/image`);
+        if(imgData) {
+            birdRef.imageLink = imgData.data.imageLink;
         }
-    
-        async function getAudio(sciName, birdRef) {
-            const audioData = await axios.get(`https://pic-beak-backend.herokuapp.com/api/v1/birds/${sciName}/audio`);
-            if(audioData) {
-                birdRef.audioLink = audioData.data.audioLink;
-            }
-        }
+    }
 
+    async function getAudio(sciName, birdRef) {
+        const audioData = await axios.get(`https://pic-beak-backend.herokuapp.com/api/v1/birds/${sciName}/audio`);
+        if(audioData) {
+            birdRef.audioLink = audioData.data.audioLink;
+        }
+    }
+    useEffect(() => {
         const getBirds = async () => {
             const mainResponse = await axios.get(
                 'https://pic-beak-backend.herokuapp.com/api/v1/birds/?page= 0&recordsPerPage= 5&subnation=BC'
@@ -42,9 +43,9 @@ function MatchView() {
                     birdsData.forEach(bird => {
                         const sciName = bird.sciName;
                         getImage(sciName, bird);
-                        getAudio(sciName, bird);
+                        // getAudio(sciName, bird);
                     });
-                    // setBirdData(mainResponse.data);
+                //     // setBirdData(mainResponse.data);
                 }
                 setBirdData(mainResponse.data);
          
@@ -52,7 +53,7 @@ function MatchView() {
                 // setBirdData(allData);
             };
             getBirds();
-        }, [birdsData]);
+        }, [birdsData.length]);
         
     console.log(birdsData[0]);
 
@@ -66,15 +67,19 @@ function MatchView() {
                 <Swiper navigation={true} modules={[Navigation]} className="mySwiper" key={birdsData}>
                 {birdsData?.map((data, index) => (
                 <SwiperSlide key={index}> 
-                    <MatchCard 
-                        imageLink={data.imageLink}
-                        alt={data.commonName}
-                        id={data.sciName}
-                        sciName={data.sciName}
-                        audioLink={data.audioLink}
-                        // function={displayBirdDetails}
-                        commonName={data.commonName}
-                    />
+                <div className='matchViewCard' id={`${data.sciName.replace(/\s/g, '-')}-card`}>
+                    <img src={data.imageLink} alt={data.commonName}/>
+                    <div className="matchDetailCard">
+                        <div className="nameContainer">
+                            <h2>{data.commonName}</h2>
+                            <p>{data.sciName}</p>
+                        </div>
+                        <div className="buttonContainer">
+                            <Audio src={data.audioLink}/>
+                            <Link to="/details" state = {{ from: '/match' }} element={<DetailDataDisplay />}><Button className='primary matchCardBtn'>This is the one!</Button></Link>
+                        </div>
+                    </div>
+                </div>
                 </SwiperSlide>
                 ))}
             </Swiper>
@@ -84,7 +89,20 @@ function MatchView() {
             <a href="/listview"><Button className="terciary changeViewBtn">Switch to list view</Button></a>
         </div>
         );
-}
+    }
+    
+    
+    // <MatchCard 
+    //     // onChange={setBirdData(birdsData)}
+    //     thisKey={index}
+    //     imageLink={data.imageLink}
+    //     alt={data.commonName}
+    //     id={data.sciName}
+    //     sciName={data.sciName}
+    //     audioLink={data.audioLink}
+    //     // function={displayBirdDetails}
+    //     commonName={data.commonName}
+    // />
     
 
 export default MatchView; 
