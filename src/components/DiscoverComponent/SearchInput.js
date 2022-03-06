c128c5fc9a4e7b629d13c7ca831f652cc85d0a59
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-03-06 14:20:32 +0900

    set close button functional

--- src/components/DiscoverComponent/SearchInput.js
+++ src/components/DiscoverComponent/SearchInput.js
@@ -2,26 +2,41 @@ import React, { useState } from "react";
 import "./SearchInput.scss";
 import FilterIcon from "./DiscoverIcons/filterGreen.svg";
 import Button from "../shared/ButtonComponent/Button";
+import seachIcon from "./DiscoverIcons/search.svg";
 
 function SearchInput({ openModal }) {
     const [close, setClose] = useState(false);
+    const [birdName, setBirdName] = useState("");
 
-    const showClose = () => {
-        if (input.length > 0) {
-            setClose((prev) => !prev);
-        }
+    const handleBirdName = (e) => {
+        setBirdName(e.target.value);
+        e.target.value.length > 0 ? setClose(true) : setClose(false);
+    };
+
+    const resetBirdName = () => {
+        setBirdName("");
+        setClose(false);
     };
 
     return (
         <div className="search-block">
             <div className="searchParent">
+                <img src={seachIcon} alt="search icon" />
                 <input
+                    onChange={handleBirdName}
+                    value={birdName}
                     className="searchBar"
                     type="text"
                     id="searchInput"
                     placeholder="enter bird name"
                 />
-                <Button className="exit"></Button>
+                {!close ? null : (
+                    <Button
+                        className="exit"
+                        close={close}
+                        onClick={resetBirdName}
+                    ></Button>
+                )}
             </div>
             <button onClick={openModal}>
                 <img
