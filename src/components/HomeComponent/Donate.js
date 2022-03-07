4b7fdcef942c9e668b75dd687baceb63d00bdaa8
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-03-07 11:26:33 +0900

    update desktop style

--- src/components/HomeComponent/Donate.js
+++ src/components/HomeComponent/Donate.js
@@ -2,24 +2,32 @@ import React from "react";
 import Button from "../shared/ButtonComponent/Button";
 import "./HomeScss/Donate.scss";
 import DonateImg from "./HomeImg/donate.png";
-import Vector from "./HomeImg/donateVector.png";
+// import Vector from "./HomeImg/donateVector.png";
+import VectorLarge from "./HomeImg/donateVectorLarge.png";
 
 function Donate(props) {
     return (
         <div className="Donate">
             <div className="imgContainer">
                 <img className="front" src={DonateImg} alt="donate block" />
-                <img className="bottom" src={Vector} alt="line behind birds" />
+                <img
+                    className="bottom"
+                    src={VectorLarge}
+                    alt="line behind birds"
+                />
             </div>
-
-            <h2>How you can help to conserve birds</h2>
-            <p>
-                By considering a gift for the birds by donating to birds
-                conservation organizations, you are also getting back to every
-                living thing including us!
-            </p>
-            <div className="buttonParent">
-                <Button className="primary">Donate</Button>
+            <div className="donate-desc">
+                <h2>How you can help conserve birds</h2>
+                <p>
+                    By considering a gift for the birds by donating to birds
+                    conservation organizations, you are also getting back to
+                    every living thing including us!
+                </p>
+                <div className="buttonParent">
+                    <a href="/donate">
+                        <Button className="primary">Donate</Button>
+                    </a>
+                </div>
             </div>
         </div>
     );
--- src/components/HomeComponent/Population.js
+++ src/components/HomeComponent/Population.js
@@ -5,15 +5,17 @@ import SpeciesChart from "./SpeciesChart";
 function Population() {
     return (
         <div className="birdPopulation">
-            <h2>
-                Monitoring the decline of birds population for conservation
-                action
-            </h2>
-            <p>
-                Tracking Canada's birds helps to identify the impacts of
-                environmental changes and provide guidance to set priorities to
-                the recovery of species at risk.
-            </p>
+            <div className="description">
+                <h2>
+                    Monitoring the decline of birds population for conservation
+                    action
+                </h2>
+                <p>
+                    Tracking Canada's birds helps to identify the impacts of
+                    environmental changes and provide guidance to set priorities
+                    to the recovery of species at risk.
+                </p>
+            </div>
             <SpeciesChart />
         </div>
     );
