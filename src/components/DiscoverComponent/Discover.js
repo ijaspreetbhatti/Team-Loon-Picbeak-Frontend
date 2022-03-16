a4233a18a8cc4aef69787d3f015aaacf8827269b
olimarie21 <olivia.underdah@gmail.com>
2022-03-16 10:29:53 -0700

    adjustments to filter

--- src/components/DiscoverComponent/Discover.js
+++ src/components/DiscoverComponent/Discover.js
@@ -1,148 +1,19 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useEffect, useState } from "react";
 import SearchInput from "./SearchInputComponent/SearchInput";
-import Filter from "./FilterComponent/Filter";
 import "./Discover.scss";
 import BirdMatchCard from "../shared/MatchCardComponent/BirdMatchCard";
+import Button from "../shared/ButtonComponent/Button";
+import "./FilterComponent/Filter.scss";
+import axios from "axios";
+// import Filter from "./FilterComponent/Filter";
 
 const Discover = () => {
-    const birdData = [
-        {
-            commonName: "Varied Thrush",
-            sciName: "Ixoreus naevius",
-            imageLink: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-            audioLink:
-                "https://xeno-canto.org/sounds/uploaded/JHFICMRVUX/XC604686-180327_02%20Varied%20Thrush.mp3",
-            description: "lorem ipsum dolor set",
-            conservationStatus: "Low Concern",
-            gallery: [
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-            ],
-        },
-        {
-            commonName: "Golden-crowned Kinglet",
-            sciName: "Regulus satrapa",
-            imageLink: `https://live.staticflickr.com/65535/51858746931_a65d3e7c77.jpg`,
-            audioLink:
-                "https://xeno-canto.org/sounds/uploaded/YQNGFTBRRT/XC500965-GCKI_Baldy_2Sep2014_Harter_01.mp3",
-            description: "lorem ipsum dolor set",
-            conservationStatus: "Low Concern",
-            gallery: [
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-            ],
-        },
-        {
-            commonName: "Glaucous-winged Gull",
-            sciName: "Larus glaucescens",
-            imageLink: `https://live.staticflickr.com/65535/51870485691_0549c81369.jpg`,
-            audioLink:
-                "https://xeno-canto.org/sounds/uploaded/SFRRHMLGSK/XC612331-Glaucous%20winged%20Gull.mp3",
-            description: "lorem ipsum dolor set",
-            conservationStatus: "Low Concern",
-            gallery: [
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-            ],
-        },
-        {
-            commonName: "Varied Thrush",
-            sciName: "Ixoreus naevius 2",
-            imageLink: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-            audioLink:
-                "https://xeno-canto.org/sounds/uploaded/JHFICMRVUX/XC604686-180327_02%20Varied%20Thrush.mp3",
-            description: "lorem ipsum dolor set",
-            conservationStatus: "Low Concern",
-            gallery: [
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-            ],
-        },
-        {
-            commonName: "Golden-crowned Kinglet",
-            sciName: "Regulus satrapa 2",
-            imageLink: `https://live.staticflickr.com/65535/51858746931_a65d3e7c77.jpg`,
-            audioLink:
-                "https://xeno-canto.org/sounds/uploaded/YQNGFTBRRT/XC500965-GCKI_Baldy_2Sep2014_Harter_01.mp3",
-            description: "lorem ipsum dolor set",
-            conservationStatus: "Low Concern",
-            gallery: [
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-                {
-                    collectedBirdImage: `https://live.staticflickr.com/65535/51847099343_15c437f1fa.jpg`,
-                },
-            ],
-        },
-    ];
-
     const [showModal, setShowModal] = useState(false);
-
-    /** additional code ****************************************/
     const [close, setClose] = useState(false);
     const [birdName, setBirdName] = useState("");
+    const [status, setStatus] = useState(null);
+    const [prov, setProv] = useState("");
+    const [birds, setBirds] = useState([]);
 
     const handleBirdName = (e) => {
         setBirdName(e.target.value);
@@ -153,21 +24,168 @@ const Discover = () => {
         setBirdName("");
         setClose(false);
     };
-    /************************************************************* */
 
     const openModal = () => {
         setShowModal((prev) => !prev);
     };
 
+    const keyPress = useCallback(
+        (e) => {
+            if (e.key === "Escape" && showModal) {
+                setShowModal(false);
+            }
+        },
+        [setShowModal, showModal]
+    );
+
+    useEffect(() => {
+        document.addEventListener("keydown", keyPress);
+        return () => document.removeEventListener("keydown", keyPress);
+    }, [keyPress]);
+    
+    /********************************************************* */
+    /** Close Modal When Background Clicked ******************* */
+    const modalRef = useRef();
+
+    const closeModal = (e) => {
+        if (modalRef.current === e.target) {
+            setShowModal(false);
+            // setShowModal((prev) => !prev);
+        }
+    };
+    /********************************************************* */
+
+    const radioDeselection = () => {
+        for (const element of document.getElementsByName("status")) {
+            element.checked = false;
+        }
+    };
+
+    const fliterBirds = () => {
+        let url;
+        
+        if(birdName && prov !== "" && status !== "") {
+            url = `https://pic-beak-backend.herokuapp.com/api/v1/birds/?page=0&recordsPerPage=25&subnation=${prov}&conservationStatus=${status}&searchKeyword=${birdName}`;
+            console.log(birdName);
+        } else if(birdName) {
+            url = `https://pic-beak-backend.herokuapp.com/api/v1/birds/?page=0&recordsPerPage=25&searchKeyword=${birdName}`;
+        } else {
+            url = `https://pic-beak-backend.herokuapp.com/api/v1/birds/?page=0&recordsPerPage=25&subnation=${prov}&conservationStatus=${status}`;
+        }
+
+        axios
+            .get(
+                `${url}`
+            )
+            .then((response) => {
+                if (response) {
+                    setBirds(response.data);
+                    console.log(birds);
+                }
+            })
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        fliterBirds()
+    }, [status]); 
+
+    console.log(birds);
+
+    const handleChange = (e) => {
+        setStatus(e.target.value)
+    };
+
     return (
         <div className="discover">
-            <Filter
-                showModal={showModal}
-                openModal={openModal}
-                setShowModal={setShowModal}
-                birdName={birdName}
-            />
-
+        <div className="Filter">
+            {showModal ? (
+                <div>
+                    <div
+                        className="filterBackground"
+                        ref={modalRef}
+                        onClick={closeModal}
+                    ></div>
+                    <div className="modalWrapper">
+                        <div className="titleWrapper">
+                            <p>Filters</p>
+                            <div
+                                className="closeIcon"
+                                onClick={openModal}
+                            ></div>
+                        </div>
+                        <form>
+                            <div className="fieldSets">
+                                <fieldset>
+                                    {/** STATUS */}
+                                    <legend>By conservation status</legend>
+                                    <div className="option">
+                                        <input
+                                            className="visually-hidden"
+                                            id="low"
+                                            type="radio"
+                                            value="G5"
+                                            name="status"
+                                        onChange={(e) =>
+                                                handleChange(e)
+                                            }
+                                        />
+                                        <label htmlFor="low">
+                                            Low conservation concern
+                                        </label>
+                                    </div>
+                                    <div className="option">
+                                        <input
+                                            className="visually-hidden"
+                                            id="moderate"
+                                            type="radio"
+                                            value="G4"
+                                            name="status"
+                                            onChange={(e) =>
+                                                handleChange(e)
+                                            }
+                                        />
+                                        <label htmlFor="moderate">
+                                            Moderate conservation concern
+                                        </label>
+                                    </div>
+                                    <div className="option">
+                                        <input
+                                            className="visually-hidden"
+                                            id="high"
+                                            type="radio"
+                                            value="G2"
+                                            name="status"
+                                            onChange={(e) =>
+                                                handleChange(e)
+                                            }
+                                        />
+                                        <label htmlFor="high">
+                                            High conservation concern
+                                        </label>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div className="btn-container">
+                                <Button
+                                    className="secondary"
+                                    onClick={radioDeselection}
+                                >
+                                    Clear
+                                </Button>
+                                <button
+                                    className="primary"
+                                    ref={modalRef}
+                                    onClick={closeModal}
+                                >
+                                    Apply
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            ) : null}
+        </div>
             <h3>Discover bird species</h3>
             <p>
                 Explore birds species and start your own birdwatching session,
@@ -181,7 +199,7 @@ const Discover = () => {
                 close={close}
             />
             <div className="cardParent">
-                {birdData.map((data) => (
+                {birds.map((data) => (
                     <BirdMatchCard
                         key={data.sciName}
                         id={data.sciName}
@@ -190,6 +208,7 @@ const Discover = () => {
                         alt={data.commonName}
                         commonName={data.commonName}
                         sciName={data.sciName}
+                        data={data}
                     />
                 ))}
             </div>
@@ -198,4 +217,3 @@ const Discover = () => {
 };
 
 export default Discover;
-
