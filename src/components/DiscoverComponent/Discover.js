cde783cbefa258d915438e90d725892c893594f9
olimarie21 <olivia.underdah@gmail.com>
2022-03-28 15:23:04 -0700

    fix merge error

--- src/components/DiscoverComponent/Discover.js
+++ src/components/DiscoverComponent/Discover.js
@@ -91,102 +91,13 @@ const Discover = (props) => {
 
     return (
         <div className="discover">
-            <div className="Filter">
-                {showModal ? (
-                    <div>
-                        <div
-                            className="filterBackground"
-                            onClick={(e) => closeModal(e)}
-                        ></div>
-                        <div className="modalWrapper">
-                            <div className="titleWrapper">
-                                <p>Filters</p>
-                                <div
-                                    className="closeIcon"
-                                    onClick={closeModal}
-                                ></div>
-                            </div>
-                            <form>
-                                <div className="fieldSets">
-                                    <fieldset>
-                                        {/** STATUS */}
-                                        <legend>By conservation status</legend>
-                                        <div className="option">
-                                            <input
-                                                className="visually-hidden"
-                                                id="low"
-                                                type="checkbox"
-                                                value="G5"
-                                                name="status"
-                                                onChange={(e) =>
-                                                    handleChange(e)
-                                                }
-                                                checked={
-                                                    check.notifyFrequency ===
-                                                    "G5"
-                                                }
-                                                onClick={handleFrequencyChange}
-                                            />
-                                            <label htmlFor="low">
-                                                Low conservation concern
-                                            </label>
-                                        </div>
-                                        <div className="option">
-                                            <input
-                                                className="visually-hidden"
-                                                id="moderate"
-                                                type="checkbox"
-                                                value="G4"
-                                                name="status"
-                                                onChange={(e) =>
-                                                    handleChange(e)
-                                                }
-                                                checked={
-                                                    check.notifyFrequency ===
-                                                    "G4"
-                                                }
-                                                onClick={handleFrequencyChange}
-                                            />
-                                            <label htmlFor="moderate">
-                                                Moderate conservation concern
-                                            </label>
-                                        </div>
-                                        <div className="option">
-                                            <input
-                                                className="visually-hidden"
-                                                id="high"
-                                                type="checkbox"
-                                                value="G3"
-                                                name="status"
-                                                onChange={(e) =>
-                                                    handleChange(e)
-                                                }
-                                                checked={
-                                                    check.notifyFrequency ===
-                                                    "G3"
-                                                }
-                                                onClick={handleFrequencyChange}
-                                            />
-                                            <label htmlFor="high">
-                                                High conservation concern
-                                            </label>
-                                        </div>
-                                    </fieldset>
-                                </div>
-                                <div className="btn-container">
-                                    <Button className="secondary">Clear</Button>
-                                    <button
-                                        className="primary"
-                                        onClick={(e) => filterBirds(e)}
-                                    >
-                                        Apply
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                ) : null}
-            </div>
+            <Filter
+                handleChange={handleChange}
+                filterBirds={filterBirds}
+                showModal={showModal}
+                setShowModal={setShowModal}
+            />
+
             <h2>Discover bird species</h2>
             <h6>
                 Explore birds species and start your own birdwatching session,
